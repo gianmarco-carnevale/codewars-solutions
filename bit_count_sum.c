f2324ef6cd887551966fa0d16ad1a6fb7d223dbf
Gianmarco Carnevale <gcarneva@qti.qualcomm.com>
2018-10-29 17:45:17 +0000

    recursive!

--- bit_count_sum.c
+++ bit_count_sum.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
-static int getBitSum(int n)
+static long long getBitSum(int n)
 {
-  int i,sum;
+  int i;
+  long long sum;
   for(i=0,sum=0;i<32;i++)
   {
     sum += (n & (1<<i))?1:0;
@@ -10,20 +11,10 @@ static int getBitSum(int n)
   return sum;
 }
 
-
-static int getCumulativeSum(int n)
+static long long getRawRangeSum(int a, int b)
 {
-  int i,sum;
-  for (i=1,sum=0;i<=n;i++)
-  {
-    sum+=getBitSum(i);
-  }
-}
-
-
-static int getRawRangeSum(int a, int b)
-{
-  int i,sum;
+  int i;
+  long long sum;
   if (b<a)
   {
     printf("ERROR: invalid range\n");
@@ -36,88 +27,37 @@ static int getRawRangeSum(int a, int b)
   return sum;
 }
 
-static int getRangeSum(int , int );
-
-static int getEvenRangeSum(int a,int b)
+static long long getRecursiveRangeSum(int a, int b)
 {
-  int difference,nibble,i,m,partial,ratio;
-  difference = b - a;
-  for (m=0xF,nibble=0x1;;m<<=4,nibble<<=4)
-  {
-    if (difference & m)
-    {
-      break;
-    }
-  }
-  ratio = difference/nibble;
-  printf("%x,%x,ratio==%i\n",a,b,ratio);
-  for (partial=0,i=0;i<ratio;i++,a+=nibble)
-  {
-    partial += getRangeSum(a, a+nibble);
-    if (i<ratio-1)
-      partial -= getBitSum(a+nibble);
-    printf("[%x,%x]\n",a, a+nibble);
-  }
-  return partial;
-}
-
-
-static int getRangeSum(int a, int b)
-{
-  int delta, difference,i,j,m,nibble,partial;
+  int middle,difference;
+  long long result;
   if (b<a)
   {
     printf("ERROR: invalid range\n");
     return -1;
   }
-  delta = 1 + a + getBitSum(a);
+  printf("[%d,%d]\n",a,b);
   difference = b - a;
-/*
-  printf("difference==%i, delta==%i\n",difference,delta);
-*/
-  switch (difference)
+  if (difference<0x100000)
+    return (long long)getRawRangeSum(a,b);
+  else
   {
-    case 0:
-      return getBitSum(a);
-    case 0x1:
-      return getBitSum(a) + getBitSum(b);
-    case 0x10:
-      return 0x20 + delta;
-    case 0x100:
-      return 0x400 + delta;
-    case 0x1000:
-      return 0x6000 + delta;
-    case 0x10000:
-      return 0x80000 + delta;
-    case 0x100000:
-      return 0xa00000 + delta;
-    case 0x1000000:
-      return 0xc000000 + delta;
-    case 0x10000000:
-      return 0xe0000000 + delta;
-
-    default:
-    {
-      return -1;
-    }
-    break;
+    middle = ((long long)a + (long long)b)/2;
+    result = getRecursiveRangeSum(a,middle) + getRecursiveRangeSum(middle,b) - getBitSum(middle);
   }
-  return -1;
 }
+
+
+
+
 
 
 int main(int argc, char* argv[])
 {
-  int a,b,c,s1, s2,s3;
-  a = 0x123;
-  b = a + 0x20000;
+  int a,b,c,s1, s2,s3,i,j,prev;
+  a = 15;
+  prev = 0;
 
-  s1 = getRawRangeSum(a,b);
-  s2 = getRangeSum(a,b);
-  s3 = getEvenRangeSum(a,b);
-  printf("----------------\n");
-  printf("%x\n",s1);
-  printf("%x\n",s2);
-  printf("%x\n",s3);
+  printf("%lli\n",getRecursiveRangeSum(0x1,0x10000000));
   return 0;
 }
