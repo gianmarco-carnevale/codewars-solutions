ff9528ee31f348f0b7ef0dbed9daacf0afebbe5b
Gianmarco Carnevale <gianmarco.carnevale@gmail.com>
2019-04-22 08:46:53 +0100

    it works but it's too slow

--- center-of-attention.c
+++ center-of-attention.c
@@ -1,156 +1,235 @@
 #include <stdio.h>
+#include <stdlib.h>
 /*---------------------------------------------------------------------*/
 typedef struct { unsigned *pixels; unsigned width, height; } Image;
 typedef struct { unsigned *values; unsigned size; } unsigned_array;
 unsigned_array central_pixels(Image,unsigned);
 /*---------------------------------------------------------------------*/
 
-struct picture_row
+static Image getDepthImage(Image src)
 {
-  int start;
-  int length;
-  int* depths;
-};
-
-struct color_area
-{
-  unsigned color;
-  int numRows;
-  struct picture_row* pRows;
-};
-
-static int compare(void* p1, void* p2)
-{
-  unsigned a,b;
-  a = ((struct color_area*)p1)->color;
-  b = ((struct color_area*)p2)->color;
-  if (a<b) return -1;
-  if (a>b) return 1;
-  return 0;
+  Image result;
+  size_t totalSize;
+  result.width = src.width;
+  result.height = src.height;
+  totalSize = result.width * result.height;
+  result.pixels = (unsigned*)calloc(totalSize,sizeof(unsigned));
+  if (result.pixels==NULL)
+  {
+    result.width=0;
+    result.height=0;
+    printf("ERROR: calloc\n");
+  }
+  return result;
 }
 
-static struct color_area* addArea(struct color_area* pAreas, struct color_area* pNew)
+static void markBoundaries(Image src, Image dest)
 {
-  if (pAreas==NULL)
+  int i,j,first,upper,pos;
+  upper = src.width;
+  for (i=0;i<upper;++i)
   {
-    pAreas = (struct color_area*)malloc(sizeof(struct color_area));
-    if (pAreas==NULL)
+    dest.pixels[i]=1;
+  }
+  first = src.width * (src.height - 1);
+  upper = src.width * src.height;
+  for (i=first;i<upper;++i)
+  {
+    dest.pixels[i]=1;
+  }
+  upper = src.height - 1;
+  for (i=1;i<upper;++i)
+  {
+    pos = i*src.width;
+    dest.pixels[pos]=1;
+    dest.pixels[pos + src.width - 1] = 1;
+  }
+}
+
+static void markEdges(Image src, Image dest)
+{
+  int i,j,first,upper,pos;
+  for (i=1;i<src.height-1;++i)
+  {
+    for (j=1;j<src.width-1;++j)
     {
-      printf("ERROR: malloc failed\n");
+      pos = i * src.width + j;
+      if (dest.pixels[pos]==0)
+      {
+        if (src.pixels[pos]!=src.pixels[pos+1])
+        {
+          dest.pixels[pos]=1;
+          dest.pixels[pos+1]=1;
+        }
+        if (src.pixels[pos]!=src.pixels[pos-1])
+        {
+          dest.pixels[pos]=1;
+          dest.pixels[pos-1]=1;
+        }
+        if (src.pixels[pos]!=src.pixels[pos+src.width])
+        {
+          dest.pixels[pos]=1;
+          dest.pixels[pos+src.width]=1;
+        }
+        if (src.pixels[pos]!=src.pixels[pos-src.width])
+        {
+          dest.pixels[pos]=1;
+          dest.pixels[pos-src.width]=1;
+        }
+      }
+    }
+  }
+}
+
+
+unsigned* addPosition(unsigned *p, unsigned pos, unsigned *pSize)
+{
+  static unsigned size=0;
+  if (p==NULL)
+  {
+    p=(unsigned*)malloc(sizeof(unsigned));
+    if (p==NULL)
+    {
+      printf("ERROR: malloc 2\n");
       return NULL;
     }
-    pAreas->color = pNew->color;
-
+    size = 1;
+    *pSize=size;
+    p[0] = pos;
+    return p;
   }
   else
-  {}
-}
-
-static struct color_area* findArea(struct color_area* pAreas, unsigned color)
-{
-  struct color_area dummy;
-  dummy->color = color;
-  return bsearch(&dummy);
-}
-
-static void addRow(struct color_area* pArea, int startPosition, int rowLength)
-{
-  if (pArea==NULL)
   {
-    printf("ERROR: pArea is null\n");
-    return;
-  }
-
-}
-
-static struct color_area* getColorAreas(Image image, int* pLength)
-{
-  struct color_area** pResult,*temp;
-  int i,j,startPosition,size,numAreas;
-  unsigned lastColor,currentColor;
-  for (pResult=NULL,numAreas=0,i=0;i<image.height;i++)
-  {
-    for (j=0;j<image.width;j++)
+    size+=1;
+    p = (unsigned*)realloc(p,size*sizeof(unsigned));
+    if (p==NULL)
     {
-      currentColor = image.pixels[(i*image.width)+j];
-      if ((i==0)&&(j==0))
+      printf("ERROR: malloc 3\n");
+      return NULL;
+    }
+    p[size-1]=pos;
+    *pSize=size;
+    return p;
+  }
+}
+
+static unsigned* findDepthOne(Image src, Image dest, unsigned colour, unsigned* pCount)
+{
+  int i,j,pos;
+  unsigned* result=NULL;
+  unsigned count = 0;
+  for (i=0;i<src.height;++i)
+  {
+    for (j=0;j<src.width;++j)
+    {
+      pos = i * src.width + j;
+      if (src.pixels[pos]==colour)
       {
-        lastColor = currentColor;
-        startPosition = 0;
-        size=1;
-      }
-      else
-      {
-        if (lastColor == currentColor)
+        if (dest.pixels[pos]==1)
         {
-          size++;
-        }
-        else
-        {
-          temp = findArea(pResult,currentColor);
-          if (temp==NULL)
-          {
-            temp = (struct color_area*)malloc(sizeof(struct color_area));
-            if (temp==NULL)
-            {
-              printf("ERROR: malloc failed.\n");
-              *pLength=0;
-              return NULL;
-            }
-            temp->color = currentColor;
-            pResult = addArea(pResult,temp);
-            numAreas++;
-          }
-          addRow(temp,startPosition,size);
-          startPosition=j;
-          size=1;
-          lastColor = currentColor;
+          result = addPosition(result,pos,&count);
         }
       }
     }
   }
-  *pLength = numAreas;
-  return pResult;
+  pCount[0]=count;
+  return result;
 }
 
 
-static void getHorizontalDepth(struct color_area* pArea)
+static unsigned* markDepth(Image src, Image dest, unsigned colour, unsigned depth, unsigned* pCount)
 {
-  int i,j;
-  for (i=0;i<pArea->numRows;i++)
+  int i,j,pos;
+  unsigned* result=NULL;
+  unsigned count = 0;
+  for (i=1;i<src.height-1;++i)
   {
+    for (j=1;j<src.width-1;++j)
+    {
+      pos = i * src.width + j;
 
+      if (dest.pixels[pos]==0)
+      {
+        if (src.pixels[pos] == colour)
+        {
+          if ((dest.pixels[pos+1]==depth-1) ||
+              (dest.pixels[pos-1]==depth-1) ||
+              (dest.pixels[pos+src.width]==depth-1) ||
+              (dest.pixels[pos-src.width]==depth-1))
+          {
+            dest.pixels[pos]=depth;
+            result = addPosition(result,pos,&count);
+          }
+        }
+      }
+    }
   }
+  pCount[0]=count;
+  return result;
 }
 
-
-static unsigned_array getMaxDepthPositions(struct color_area* pArea, unsigned width, unsigned height)
-{
-
-}
 
 unsigned_array central_pixels(Image image, unsigned colour)
 {
-  int numAreas;
   unsigned_array result;
-  static struct color_area* pColorAreas=NULL;
-  numAreas=0;
-  if (pColorAreas==NULL)
+  Image depthImage;
+  unsigned i,maxDepth,count;
+  unsigned* temp;
+  if ((image.width<3) || (image.height<3))
   {
-    pColorAreas = getColorAreas(image,&numAreas);
-    if (pColorAreas==NULL)
-    {
-      printf("ERROR: failed to find color areas.\n");
-      result.values=NULL;
-      result.size=0;
-      return result;
-    }
-
+    result.values = (unsigned*)malloc(sizeof(unsigned));
+    result.values[0]=1;
+    if ((image.width<1) || (image.height<1))
+      result.size = 0;
+    else
+      result.size = 1;
+    return result;
   }
+  depthImage = getDepthImage(image);
+  if (depthImage.pixels==NULL)
+  {
+    printf("ERROR: malloc\n");
+    return result;
+  }
+  markBoundaries(image,depthImage);
+  markEdges(image,depthImage);
+  maxDepth = (image.height<image.width)?image.height:image.width;
+  if (maxDepth%2)
+    maxDepth = 1+maxDepth/2;
+  else
+    maxDepth = maxDepth/2;
+  result.values=findDepthOne(image,depthImage,colour,&result.size);
+  for (i=2;i<=maxDepth;++i)
+  {
+    temp = markDepth(image,depthImage,colour,i,&count);
+    if (temp==NULL)
+      break;
+    else
+    {
+      if (result.values)
+        free(result.values);
+      result.size = count;
+      result.values = temp;
+    }
+  }
+  if (result.values==NULL)
+  {
+    result.values=(unsigned*)malloc(sizeof(unsigned));
+  }
+  return result;
 }
 
 
 int main(int argc, char* argv[])
 {
+  unsigned image_data[] = {1,1,4,4,4,4,2,2,2,2,
+                         1,1,1,1,2,2,2,2,2,2,
+                         1,1,1,1,2,2,2,2,2,2,
+                         1,1,1,1,1,3,2,2,2,2,
+                         1,1,1,1,1,3,3,3,2,2,
+                         1,1,1,1,1,1,3,3,3,3};
+Image image = { image_data, 10, 6 };
+unsigned_array expected = central_pixels(image,3);
+
   return 0;
 }
